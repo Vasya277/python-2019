6e33516c8a1c96eba70ed9af2761924f97663bb3
Vasya277 <31722005+Vasya277@users.noreply.github.com>
2019-12-15 21:36:27 +0200

    Add files via upload

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,31 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+import os
+from flask_admin import Admin
 
 app  = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisisasecret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'site.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+migrate = Migrate(app, db)
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+import flaskblog.forms as views
+admin = Admin(app)
+
+admin.add_view(views.UserAdminView(views.User, db.session))
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User, CKTextAreaField
+from flask_login import current_user
+from flask_wtf.file import FileField, FileAllowed
+from flask_admin.contrib.sqla import ModelView
+from flask_admin.actions import ActionsMixin
+from flask_admin.form import rules
+from flaskblog import bcrypt
 
 class RegistrationForm(FlaskForm):
 	username = StringField('Username', validators=[DataRequired(), Length(min=2,max=20)])
@@ -9,6 +16,17 @@ class RegistrationForm(FlaskForm):
 	confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
 	submit = SubmitField('Sign up')
 
+	def validate_username(self, username):
+		user = User.query.filter_by(username = username.data).first()
+		if user:
+			raise ValidationError('That username is taken. Choose a different one, please.')
+
+	def validate_email(self, field):
+		user = User.query.filter_by(email = field.data).first()
+		if user:
+			raise ValidationError('Email is already registered.')
+
+
 
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
@@ -16,3 +34,93 @@ class LoginForm(FlaskForm):
 	remember = BooleanField('Remember Me')
 	submit = SubmitField('Login')
 
+
+class UpdateAccountForm(FlaskForm):
+	username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+	email = StringField('Email', validators=[DataRequired(), Email()])
+	picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+	about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+	old_pass = PasswordField('Old password')
+	new_pass = PasswordField('New password')
+	confirm_pass = PasswordField('Confirm password', validators=[EqualTo('new_pass')])
+	submit = SubmitField('Update')
+
+	def validate_username(self, username):
+		if username.data != current_user.username:
+			user = User.query.filter_by(username=username.data).first()
+			if user:
+				raise ValidationError('That username is taken. Please choose a different one.')
+
+	def validate_email(self, email):
+		if email.data != current_user.email:
+			user = User.query.filter_by(email=email.data).first()
+			if user:
+				raise ValidationError('That email is taken. Please choose a different one.')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Submit')
+
+class AdminUserCreateForm(FlaskForm):
+    username = StringField('Username', [DataRequired()])
+    password = PasswordField('Password', [DataRequired()])
+    admin = BooleanField('Is Admin ?')
+
+
+class AdminUserUpdateForm(FlaskForm):
+    username = StringField('Username', [DataRequired()])
+    admin = BooleanField('Is Admin ?')
+
+
+class UserAdminView(ModelView, ActionsMixin):
+	column_searchable_list = ('username',)
+	column_sortable_list = ('username', 'admin')
+	column_exclude_list = ('password',)
+	form_excluded_columns = ('password',)
+	form_edit_rules = ('username', 'admin',)
+
+
+	form_edit_rules = ('username', 'admin', 'notes', rules.Header('Reset Password'), 'new_password', 'confirm')
+	form_create_rules = ('username', 'admin', 'email','notes', 'password')
+
+	form_overrides = dict(notes=CKTextAreaField)
+	create_template = 'edit.html'
+	edit_template = 'edit.html'
+
+	def is_accessible(self):
+		return current_user.is_authenticated and current_user.is_admin()
+
+	def scaffold_form(self):
+		form_class = super(UserAdminView, self).scaffold_form()
+		form_class.password = PasswordField('Password')
+		form_class.new_password = PasswordField('New Password')
+		form_class.confirm = PasswordField('Confirm New Password')
+		return form_class
+
+	def create_model(self, form):
+		model = self.model(
+			form.username.data, form.password.data, form.admin.data
+		)
+		form.populate_obj(model)
+		model.password = bcrypt.generate_password_hash(form.password.data)
+		self.session.add(model)
+		self._on_model_change(form, model, True)
+		self.session.commit()
+
+	def update_model(self, form, model):
+		form.populate_obj(model)
+		if form.new_password.data:
+			if form.new_password.data != form.confirm.data:
+				flash('Passwords must match')
+				return
+			model.password = bcrypt.generate_password_hash(form.new_password.data)
+		self.session.add(model)
+		self._on_model_change(form, model, False)
+		self.session.commit()
